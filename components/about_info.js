21708751dcd8089f4b022d51ba8c578d8f8e2671
Cristobal Matute <cristobalmatu7@gmail.com>
2023-02-16 11:20:29 -0500

    Updated  About info

--- components/about_info.js
+++ components/about_info.js
@@ -2,5 +2,5 @@ export const about = {
   image: "https://avatars.githubusercontent.com/u/94920636?v=4",
   title: "Here is a little Background",
   description:
-    "Dedicated and efficient full stack developer with 2+ years experience in application web, mobile and databases. Specialized in MERN Stack and beyond, increasing effectiveness of my products for the customers all around the globe. Seeking to sharpe my skills to become the best developer. ",
+    "Dedicated and efficient full stack developer with 2+ years experience in application web, mobile and databases. Specialized in MERN Stack and beyond, increasing effectiveness of my products for the customers all around the globe. Seeking to sharpen my skills to become the best developer. ",
 };
