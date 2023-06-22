49b2f04930495fe4ed1ef5c0420a4f5bb40fcb19
Cristobal Matute <cristobalmatu7@gmail.com>
2023-06-22 15:38:42 -0500

    Changed about

--- components/about_info.js
+++ components/about_info.js
@@ -2,5 +2,5 @@ export const about = {
   image: "https://avatars.githubusercontent.com/u/94920636?v=4",
   title: "Here is a little Background",
   description:
-    "I am a passionate and experienced full stack developer specializing in the MERN stack. With a strong background in JavaScript, React, Node.js, and MongoDB, I have successfully designed and implemented various web applications. My expertise extends to both frontend and backend development, and I thrive in agile environments, collaborating with cross-functional teams to deliver high-quality and user-friendly solutions. ",
+    "Full stack developer specialized in the MERN stack. With a strong background in JavaScript, React, Node.js, and MongoDB, I have successfully designed and implemented various web applications. My expertise extends to both frontend and backend development, and I thrive in agile environments, collaborating with cross-functional teams to deliver high-quality and user-friendly solutions. ",
 };
