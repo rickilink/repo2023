c4ab7645ded95ba496318718b1bf2651cd4a7578
Cristobal Matute <cristobalmatu7@gmail.com>
2023-06-22 15:52:00 -0500

    Update social_info.js

--- components/social_info.js
+++ components/social_info.js
@@ -1,37 +1,51 @@
 export const social = [
-  {
+  /*   {
     id: 1,
     network: "facebook",
     url: "https://www.messenger.com/t/100008705118875",
     color: "gray",
     bgColor: "transparent",
-  },
-  {
+  }, */
+  /* {
     id: 2,
 
     url: "https://twitter.com/cristobalmatute",
     color: "gray",
     bgColor: "transparent",
-  },
+  }, */
   {
     id: 3,
-
     url: "https://www.linkedin.com/in/cristobalmatu7/",
     color: "gray",
     bgColor: "transparent",
   },
   {
     id: 4,
-
     network: "whatsapp",
     url: "https://wa.me/51938874790",
     color: "gray",
     bgColor: "transparent",
   },
-  {
+  /*  {
     id: 5,
 
     url: "https://www.instagram.com/cristobalmatutecm/",
+    color: "gray",
+    bgColor: "transparent",
+  }, */
+  {
+    id: 6,
+
+    network: "github",
+    url: "https://github.com/rickilink",
+    color: "gray",
+    bgColor: "transparent",
+  },
+  {
+    id: 6,
+
+    network: "google",
+    url: "mailto:cristobalmatu7@gmail.com?Subject=Hello%20Cristobal!%20I%20Came%20From%20Your%20Webpage",
     color: "gray",
     bgColor: "transparent",
   },
