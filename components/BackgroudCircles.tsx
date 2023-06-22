d717c3654cb6c611214ca2969ec62af751cd6c24
Cristobal Matute <cristobalmatu7@gmail.com>
2023-06-22 01:05:00 -0400

    fixed circles

--- components/BackgroudCircles.tsx
+++ components/BackgroudCircles.tsx
@@ -10,12 +10,12 @@ export const BackgroudCircles = (props: Props) => {
         opacity: 0,
       }}
       animate={{
-        scale: [1, 2, 2, 3, 1],
-        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
-        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+        scale: [1, 2, 1],
+        opacity: [0.1, 0.2, 0.4, 1.0],
+        borderRadius: ["20%", "20%", "20%"],
       }}
       transition={{
-        duration: 2.5,
+        duration: 1,
       }}
       className="relative flex justify-center items-center"
     >
